1c8fb54ca32972f10aa7892ebc212bcf23c22ec7
Meenu Yadav <yadav.meenu2023@gmail.com>
2023-12-27 16:46:10 +0530

    make footer, header $ from tablet size complete

---
+++ header.js
@@ -0,0 +1,23 @@
+let head = document.getElementById("header")
+let headerN =
+`<div class="logo">
+            <img src="https://placehold.co/250x100/red/white?text=Logo" alt="Logo">
+        </div>
+        
+        <!-- <NavigationMenu/> -->
+        <nav>
+            <div>
+                <a href="stickers/index.html"><img src="../assets/navImages/sticker.png" alt="Stickers"></a>
+                <p>Stickers</p>
+            </div>
+            <div>
+                <a href="workshop/index.html"><img src="../assets/navImages/workshop.png" alt="Workshop"></a>
+                <p>Workshop</p>
+            </div>
+            <div>
+                <a href="contest/index.html"><img src="../assets/navImages/contest.png" alt="Contest"></a>
+                <p>Contest</p>
+            </div>
+        </nav>`
+        head.innerHTML = headerN;
+    
