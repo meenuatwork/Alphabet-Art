63873533edcd260726fa38713e9b7c96e848fff3
Meenu Yadav <yadav.meenu2023@gmail.com>
2024-02-07 22:53:10 +0530

    Create Contest Form

--- contest/contestForm.js
+++ contest/contestForm.js
@@ -1,7 +1,7 @@
 function previewImage(event) {
     var input = event.target;
     var image = document.getElementById('preview');
-    if (input.files && input.files[0]) {
+   //  if (input.files && input.files[0]) {
        var reader = new FileReader();
        reader.onload = function(e) {
           image.src = e.target.result;
@@ -9,5 +9,7 @@ function previewImage(event) {
           image.style.height = "157px"
        }
        reader.readAsDataURL(input.files[0]);
-    }
+       
+    // }
  }
+ 
